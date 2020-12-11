2f970f0d01f4f7317a49288818c07d039debeb55
Quackdratic <39701964+Quackdratic@users.noreply.github.com>
2020-12-11 11:40:04 +0200

    day 11 code cool changes

--- src/day11/main.py
+++ src/day11/main.py
@@ -1,4 +1,5 @@
 from src.helper import IO
+import itertools
 import copy
 
 
@@ -12,7 +13,7 @@ def seat_trace(pos_x, pos_y, vel_x, vel_y):
 
     pos_x += vel_x
     pos_y += vel_y
-    while 0 <= pos_x < n and 0 <= pos_y < m:
+    while pos_x in range(n) and pos_y in range(m):
         if seats[pos_x][pos_y] != '.':
             return seats[pos_x][pos_y]
         pos_x += vel_x
@@ -23,18 +24,17 @@ def seat_trace(pos_x, pos_y, vel_x, vel_y):
 n_iter = 0
 while True:
     next_seats = copy.deepcopy(seats)
-    for line in range(n):
-        for coll in range(m):
-            # adj_seats = [seats[i][j] for i in range(max(0, line-1), min(n, line+2))
-            #              for j in range(max(0, coll-1), min(m, coll+2))]
-            adj_seats = [seat_trace(line, coll, i, j) for i in range(-1, 2) for j in range(-1, 2)]
-            empty_cnt, occup_cnt = adj_seats.count('L'), adj_seats.count('#')
+    for line, coll in itertools.product(range(n), range(m)):
+        # adj_seats = [seats[i][j] for i in range(max(0, line-1), min(n, line+2))
+        #              for j in range(max(0, coll-1), min(m, coll+2))]
+        adj_seats = [seat_trace(line, coll, i, j) for i in range(-1, 2) for j in range(-1, 2)]
+        occup_cnt = adj_seats.count('#')
 
-            if seats[line][coll] == 'L' and occup_cnt == 0:
-                next_seats[line][coll] = '#'
-            # if seats[line][coll] == '#' and occup_cnt >= 5:
-            if seats[line][coll] == '#' and occup_cnt >= 6:
-                next_seats[line][coll] = 'L'
+        if seats[line][coll] == 'L' and occup_cnt == 0:
+            next_seats[line][coll] = '#'
+        # if seats[line][coll] == '#' and occup_cnt >= 5:
+        if seats[line][coll] == '#' and occup_cnt >= 6:
+            next_seats[line][coll] = 'L'
 
     if next_seats == seats:
         break
